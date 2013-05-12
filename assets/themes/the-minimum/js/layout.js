eed6fb055ed112117be85a5fea5c3e8d4a01b916
cloveryu <clover210yu@gmail.com>
2013-05-12 15:31:43 +0800

    site render

--- assets/themes/the-minimum/js/layout.js
+++ assets/themes/the-minimum/js/layout.js
@@ -1,7 +1,9 @@
 $(function() {
 	var _width = 960;
 	$("body").load(function(){
+			alert(2);
 		if (top == window) {
+			alert(1);
 		  	var winWidth = $(window).width();
 		  	var wid = (winWidth - _width) < 0 ? 0 : Math.floor((winWidth - _width) / 2);
 		  	$(".logo").attr("style", "left:" + wid + "px;");
