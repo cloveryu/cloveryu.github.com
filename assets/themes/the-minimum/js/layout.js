b1cffd7f8ea41a9c54d30b0a69fd7bdfebb37908
cloveryu <clover210yu@gmail.com>
2013-05-12 15:36:58 +0800

    site render

--- assets/themes/the-minimum/js/layout.js
+++ assets/themes/the-minimum/js/layout.js
@@ -1,13 +1,17 @@
 $(function() {
 	var _width = 960;
-	alert(3);
-	$("body").load(function(){
-			alert(2);
+	var leftAliged = function() {
+		alert(2);
 		if (top == window) {
 			alert(1);
 		  	var winWidth = $(window).width();
 		  	var wid = (winWidth - _width) < 0 ? 0 : Math.floor((winWidth - _width) / 2);
 		  	$(".logo").attr("style", "left:" + wid + "px;");
 		}
+	};
+	$(window).resize(leftAliged);
+	$("body").load(function(){
+		
 	});
+
 });
